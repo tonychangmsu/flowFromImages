ffa2f29ec7dca397149bb84420eb3cef9cf01be6
root <root@keras-vm.i0skxdh311qenelej3jrah0kjg.xx.internal.cloudapp.net>
2019-06-21 20:49:19 +0000

    added zipped data

--- python/src/analysis_tools.py
+++ python/src/analysis_tools.py
@@ -1,6 +1,49 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import itertools
+import concurrent.futures
+import os
+
+def _count_file(fn):
+    with open(fn, 'rb') as f:
+        return _count_file_object(f)
+    
+def _count_file_object(f):
+    # Note that this iterates on 'f'.
+    # You *could* do 'return len(f.read())'
+    # which would be faster but potentially memory 
+    # inefficient and unrealistic in terms of this 
+    # benchmark experiment. 
+    total = 0
+    for line in f:
+        total += len(line)
+    return total
+
+def unzip_member_f3(zip_filepath, filename, dest):
+    with open(zip_filepath, 'rb') as f:
+        zf = zipfile.ZipFile(f)
+        zf.extract(filename, dest)
+    fn = os.path.join(dest, filename)
+    return _count_file(fn)
+
+def extract_data(fn, dest):
+    with open(fn, 'rb') as f:
+        zf = zipfile.ZipFile(f)
+        futures = []
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            for member in zf.infolist():
+                futures.append(
+                    executor.submit(
+                        unzip_member_f3,
+                        fn,
+                        member.filename,
+                        dest,
+                    )
+                )
+            total = 0
+            for future in concurrent.futures.as_completed(futures):
+                total += future.result()
+    return total
 
 def plot_confusion_matrix(cm, classes, normalize=False, outname='conf_mat.png', cmap=plt.cm.Blues):
     """
--- run-jupyter.sh
+++ run-jupyter.sh
@@ -1,1 +1,1 @@
-nvidia-docker run -it --rm -v $(pwd):/contents -w /contents -p 8888:8888 -e PASSWORD=jupyter tonychangcsp/keras:latest jupyter notebook --port 8888 --ip 0.0.0.0 --no-browser --allow-root 
+nvidia-docker run -it --rm -v $(pwd):/content -w /content -p 8888:8888 -e PASSWORD=jupyter tonychangcsp/keras:latest jupyter notebook --port 8888 --ip 0.0.0.0 --no-browser --allow-root 
